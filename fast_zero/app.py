0002d1320f791d7b83e050382505b7b123c99db5
omaucosta <mauricios.costta@gmail.com>
2024-03-23 23:45:03 -0300

    feat(app): Add method post with user

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from fast_zero.schemas import Message, UserSchema, UserPublic
+
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=200, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users/', status_code=201, response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
